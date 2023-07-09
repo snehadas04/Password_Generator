6d5dc240dc99945c7b7034e66ed5e5fbb74e6514
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-09 22:47:09 +0530

    Add files via upload

---
+++ password.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+import java.util.Scanner;
+public class password {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random ran = new Random();
+        System.out.println("Enter the Password Length : ");
+        
+    }
+}
