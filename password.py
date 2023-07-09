e71378abe8ca9259d32c000f81159a997b703ed4
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-09 22:28:25 +0530

    Add files via upload

--- password.py
+++ password.py
@@ -4,3 +4,6 @@ upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 number = "0123456789"
 symbol = "[]{}<>|()=_-@#$%?&*:;,"
 join = lower + upper + number + symbol
+length = 8
+password = "".join(random.sample(join,length))
+print("Password is : ",password)
