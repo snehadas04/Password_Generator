4cc82d813afab943e0f2cf225650acf1c41590cc
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-09 22:25:10 +0530

    Add files via upload

--- password.py
+++ password.py
@@ -1,3 +1,6 @@
 import random
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbol = "[]{}<>|()=_-@#$%?&*:;,"
+join = lower + upper + number + symbol
