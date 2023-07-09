71bf8d4f9b9874bb6d19b4e4bdcc478202862bb0
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-09 22:35:47 +0530

    Randomly Password generated by the machine
    Using python a Random valid Password generated by the machine

--- password.py
+++ password.py
@@ -1,9 +1,12 @@
 import random
+
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 number = "0123456789"
 symbol = "[]{}<>|()=_-@#$%?&*:;,"
+
 join = lower + upper + number + symbol
 length = 8
 password = "".join(random.sample(join,length))
+
 print("Password is : ",password)
