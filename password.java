69d61268717d6e8e603208da90422587a1f73d24
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-09 23:05:45 +0530

    Random Numeric Password Generate by Machine
    Using Java, a random number password generated by the machine but the length of the password is dynamic according to user

--- password.java
+++ password.java
@@ -4,9 +4,11 @@ public class password {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random ran = new Random();
+
         System.out.println("Enter the Password Length : ");
         int len = sc.nextInt();
-        System.out.println("Password is : ");
+        
+        System.out.print("Password is : ");
         for(int i=0;i<len;i++){
             System.out.println(ran.nextInt(10));
         }
