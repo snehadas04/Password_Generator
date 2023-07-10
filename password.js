91d2c9b0e9ccfcbb722fd834b5b3b3573a9ab1b6
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-10 20:18:15 +0530

    Add files via upload

--- password.js
+++ password.js
@@ -1,3 +1,10 @@
 const pass_el = document.querySelector('#password span');
 const len = 8;
 const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*(){}[]/+_-"
+
+function genPassword(){
+    let pass = "";
+    for(var i=0;i<=length;i++){
+        
+    }
+}
